b40397c381f470c66cc810321f0e891417d02f39
sun yuli <sunpaq@gmail.com>
2012-12-20 16:55:54 +0900

    Update RuntimeBasedv2/Bird.c

--- RuntimeBasedv2/Bird.c
+++ RuntimeBasedv2/Bird.c
@@ -1,25 +1,25 @@
 #include "Bird.h"
 
-method_imp(Bird, bye, xxx)
+method(Bird, bye, xxx)
 {
 	//debug_log("%s\n", "Bird: goodbye");
 }
-method_imp(Bird, fly, xxx)
+method(Bird, fly, xxx)
 {
 	debug_log("%s\n", "Bird: default fly");
 }
-method_imp(Duck, fly, xxx)
+method(Bird, duck_fly, xxx)
 {
 	debug_log("%s\n", "Bird:Duck GuaGuaGua fly");
 }
-method_imp(Chicken, fly, xxx)
+method(Bird, chicken_fly, xxx)
 {
 	debug_log("%s\n", "Bird:Chicken JiJiJi fly");
 }
 
-constructor_imp(Bird, int type)
+constructor(Bird, int type)
 {
-	Chis(Bird, MCObject, nil);
+	super_init(this, MCObject, nil);
 	//bind
 	if(set_class(this, "Bird", "MCObject")){
 		bind(this, MK(fly), MV(Bird, fly));
@@ -28,11 +28,15 @@ constructor_imp(Bird, int type)
 	//override
 	if(type==DUCK_TYPE){
 		this->name="Bird:Duck";
-		override(this, MK(fly), MV(Duck, fly));
+		override(this, MK(fly), MV(Bird, duck_fly));
 	}else if(type==CHICKEN_TYPE){
 		this->name="Bird:Chicken";
-		override(this, MK(fly), MV(Chicken, fly));
+		override(this, MK(fly), MV(Bird, chicken_fly));
+	}else if(type==NONE){
+		override(this, MK(fly), MV(Bird, fly));
 	}
 	//var init
 	this->type=type;
+
+	return (Bird*)this;
 }
