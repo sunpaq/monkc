5953b152460060533e6ccac76415c9e65e4c0cac
sunpaq <sunpaq@gmail.com>
2014-01-18 04:52:39 +0900

    delete the old implements

--- old/RuntimeBased/MCClock.c
+++
@@ -1,9 +0,0 @@
-#include "MCClock.h"
-
-method_imp(MCClock, getCurrentTimeString, xxx)
-{
-	time_t timer;
-	timer=time(NULL);
-	return asctime(localtime(&timer));
-}
-
--- old/RuntimeBased/MCString.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MCString__
-#define __MCString__
-
-extern char get_one_char();
-extern void get_chars_until_enter(char resultString[]);
-
-#endif
