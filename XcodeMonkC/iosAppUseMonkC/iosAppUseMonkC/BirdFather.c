ae13f6c4c996bd2712911e2f63caf23da6255d43
sunpaq <sunpaq@gmail.com>
2014-02-15 02:33:18 +0900

    add more class

---
+++ XcodeMonkC/iosAppUseMonkC/iosAppUseMonkC/BirdFather.c
@@ -0,0 +1,9 @@
+//
+//  BirdFather.c
+//  iosAppUseMonkC
+//
+//  Created by sun yuli on 2014/02/15.
+//  Copyright (c) 2014年 sun yuli. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ XcodeMonkC/iosAppUseMonkC/iosAppUseMonkC/BirdFather.h
@@ -0,0 +1,14 @@
+//
+//  BirdFather.h
+//  iosAppUseMonkC
+//
+//  Created by sun yuli on 2014/02/15.
+//  Copyright (c) 2014年 sun yuli. All rights reserved.
+//
+
+#ifndef iosAppUseMonkC_BirdFather_h
+#define iosAppUseMonkC_BirdFather_h
+
+
+
+#endif
