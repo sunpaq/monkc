d04a202e5ee1c654009c7bd0706e43a7d38dfe51
Yuli <yu.li.sun@cn.schindler.com>
2017-07-10 14:37:48 +0800

    simplify the build script

--- example/settings.rb
+++ example/settings.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/ruby
 require 'mcbuild'
 
-libs = [
-	$com_oreisoft_mcruntime, 
-	$com_oreisoft_lemontea
-]
-
-$example = MCBuild.new(File.dirname(__FILE__))
+$example = MCBuild.new(__dir__)
 	.set_name("exp")
-	.set_dependency(libs)
+	.set_dependency([
+		$com_oreisoft_mcruntime, 
+		$com_oreisoft_lemontea])
 
 
